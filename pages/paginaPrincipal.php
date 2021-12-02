b0ff0c83f57125924cf5aca4fbf4b45409de2d53
brayanbravo <brayan.bravo.arebalo@gmail.com>
2021-12-01 22:29:12 -0300

    agregar template plantilla

--- pages/paginaPrincipal.php
+++ pages/paginaPrincipal.php
@@ -14,7 +14,7 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark  bg-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="../pages/homePage.php">Mysterest</a>
+                <a class="navbar-brand" href="../pages/paginaPrincipal.php">Mysterest</a>
                 <button class="navbar-toggler bg-danger" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -35,101 +35,50 @@
         </nav>
     </header>
 
-    <br>
-    <div class="card text-center">
-        <div class="card-header">
-
-            <button type="button" class="btn btn-white position-relative">
-                Nombre del tema
-                <span id="cantidad" class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                    3
-                </span>
-            </button>
-            <!--
-            <button>
-                <h4 class="btn-white">Nombre del tema</h4>
-                <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                    14
-                </span>
-            </button> -->
-
-
-
-        </div>
-        <div class="card-body">
-            <p>aqui debe ir la importancia del tema</p>
-        </div>
-
-        <div class="card-footer text-muted">
-            <select class="form-select form-select-sm" aria-label=".form-select-sm example">
-                <option value="1">Recientes</option>
-                <option value="2">Antiguos</option>
-                <option value="3">Mas vistos</option>
-            </select>
-            <div class="accordion" id="accordionExample">
-                <div class="accordion-item">
-                    <h2 class="accordion-header" id="headingOne">
-                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse2" aria-expanded="true" aria-controls="collapseOne">
-                            <h5> subtema con un nombre largo pero no tan largo </h5>
-                        </button>
-                    </h2>
-                    <div id="collapse2" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                        <div class="accordion-body">
-                            <span class="badge bg-danger rounded-pill">
-                                <h6>nombre usuario 1</h6>
-                            </span><br>
-                            <strong>This is the first item's accordion body.</strong> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita dicta odio rem consectetur nisi, sequi iste debitis animi error culpa ratione aperiam magnam provident architecto aliquid enim molestias quas adipisci!
-                            <cite title="Source Title">aca debe ir su cita</cite>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="accordion-item">
-                    <h2 class="accordion-header" id="headingTwo">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                            <h5> subten con un nombre largo pero no tan largo </h5>
-                        </button>
-                    </h2>
-                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                        <div class="accordion-body">
-                            <span class="badge bg-danger rounded-pill">
-                                <h6>nombre usuario 2</h6>
-                            </span><br>
-                            <strong>This is the first item's accordion body.</strong> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita dicta odio rem consectetur nisi, sequi iste debitis animi error culpa ratione aperiam magnam provident architecto aliquid enim molestias quas adipisci!
-                            <cite title="Source Title">aca debe ir su cita</cite>
-                        </div>
-                    </div>
-                </div>
-                <div class="accordion-item">
-                    <h2 class="accordion-header" id="headingThree">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                            <h5> subten con un nombre largo pero no tan largo </h5>
-                        </button>
-                    </h2>
-                    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                        <div class="accordion-body">
-                            <span class="badge bg-danger rounded-pill">
-                                <h6>nombre usuario 3</h6>
-                            </span><br>
-                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita dicta odio rem consectetur nisi, sequi iste debitis animi error culpa ratione aperiam magnam provident architecto aliquid enim molestias quas adipisci!</p>
-                            <cite title="Source Title">aca debe ir su cita</cite>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div>
-                <button class="btn btn-primary">agragar</button>
-                <button class="btn btn-primary">Ant</button>
-                <button class="btn btn-primary">Sig</button>
-                <button class="btn btn-primary">Debatir</button>
-            </div>
-
-        </div>
-    </div>
 
     <footer>
 
     </footer>
+
+    <template id="tema">
+
+        <br>
+        <div class="card text-center">
+            <div class="card-header">
+
+                <button type="button" class="btn btn-white position-relative">
+                    <h5 id="nombreTema"></h5>
+                    <span id="cantidad" class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                    </span>
+                </button>
+
+
+            </div>
+
+            <div class="card-body">
+                <p id="importancia"></p>
+            </div>
+
+            <div class="card-footer text-muted">
+                <select class="form-select form-select-sm" aria-label=".form-select-sm example">
+                    <option value="1">Recientes</option>
+                    <option value="2">Antiguos</option>
+                    <option value="3">Mas vistos</option>
+                </select>
+                <div class="accordion" id="accordionExample">
+
+                </div>
+                <div>
+                    <button class="btn btn-primary">agragar</button>
+                    <button class="btn btn-primary">Ant</button>
+                    <button class="btn btn-primary">Sig</button>
+                    <button class="btn btn-primary">Debatir</button>
+                </div>
+
+            </div>
+        </div>
+
+    </template>
 
     <template id="subtema">
 
