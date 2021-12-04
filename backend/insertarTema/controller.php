189741c078cd51bb13047e0de06239a8c563ccca
brayanbravo <brayan.bravo.arebalo@gmail.com>
2021-12-04 20:02:49 -0300

    creacion de controller y model para insertar tema

---
+++ backend/insertarTema/controller.php
@@ -0,0 +1,26 @@
+<?php
+include "model.php";
+
+$titulo = $_POST["titulo"];
+$importancia = $_POST["importancia"];
+$etiqueta1 = $_POST["etiqueta1"];
+$etiqueta2 = $_POST["etiqueta2"];
+$etiqueta3 = $_POST["etiqueta3"];
+$usuario = $_POST["usuario"];
+
+if (!empty($titulo) && !empty($importancia) && !empty($usuario)) {
+    $id = insertarTema($titulo, $importancia, $usuario, $etiqueta1, $etiqueta2, $etiqueta3);
+    if ($id =1) {
+        header("location:correcto.php");
+    } else {
+        header("location:error.php");
+    }
+  
+} else {
+    header("location:error.php");
+
+    
+}
+
+
+//header("location:../../pages/paginaPrincipal.php");
---
+++ backend/insertarTema/model.php
@@ -0,0 +1,27 @@
+<?php
+function insertarTema($titulo, $importancia, $usuario, $etiqueta1, $etiqueta2, $etiqueta3)
+{
+
+    try {
+        include "../Config.php";
+        $conn = new PDO("mysql:host=" . $servername . ";dbname=" . $database . "; port=" . $port, $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $stmt = $conn->prepare("INSERT INTO temas (titulo, importancia, id_usuario, etiqueta1, etiqueta2, etiqueta3) VALUES (:titulo, :importancia, :usuario, :etiqueta1, :etiqueta2, :etiqueta3)");
+
+        $stmt->bindParam(":titulo", $titulo);
+        $stmt->bindParam(":importancia", $importancia);
+        $stmt->bindParam(":usuario", $usuario);
+        $stmt->bindParam(":etiqueta1", $etiqueta1);
+        $stmt->bindParam(":etiqueta2", $etiqueta2);
+        $stmt->bindParam(":etiqueta3", $etiqueta3);
+
+        $stmt->execute();
+
+        $valor = $stmt->fetch(PDO::FETCH_OBJ);
+        $conn = null;
+        return $valor;
+    } catch (Exception $e) {
+        echo "conexion fallida: " . $e->getMessage();
+    }
+}
