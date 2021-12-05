d0815ae9305ddf740b68ecf922c036a53fcef4cd
brayanbravo <brayan.bravo.arebalo@gmail.com>
2021-12-05 12:07:33 -0300

    agregar validador de contendido y validador de ingreso de texto

--- pages/tema.php
+++ pages/tema.php
@@ -1,6 +1,9 @@
 <?php
-session_start();
-
+    session_start();
+    if(isset($_SESSION["user"])){
+    } else {
+        header("location:../pages/InicioSesion.php");
+    }
 ?>
 
 <!DOCTYPE html>
@@ -19,19 +22,31 @@ session_start();
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark  bg-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="../pages/homePage.php">Mysterest</a>
+                <a class="navbar-brand" href="../pages/paginaPrincipal.php">Mysterest</a>
                 <button class="navbar-toggler bg-danger" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
                         <ul class="navbar-nav">
-
-                            <li class="nav-item">
-                                <a class="nav-link active" href="">pagina 1</a>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Tema
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item" href="#">Mis Temas</a></li>
+                                    <li><a class="dropdown-item" href="#">Otros temas</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="../pages/tema.php">Iniciar Tema</a></li>
+                                </ul>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link active" href="">cuenta</a>
+                                <a class="nav-link active" href="">Mis Etiquetas</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="../pages/cerrarSesion.php">cerrar Sesion</a>
                             </li>
                         </ul>
                     </div>
@@ -52,36 +67,41 @@ session_start();
             </div>
             <br>
             <select class="form-select" name="etiqueta1" aria-label="Default select example">
-                <option selected>Sin Etiqueta</option>
-                <option value="1">Etiqueta 1</option>
-                <option value="2">Etiqueta 2</option>
-                <option value="3">Etiqueta 3</option>
+                <option value="1">Sin Etiqueta</option>
+                <option value="2">Etiqueta 1</option>
+                <option value="3">Etiqueta 2</option>
+                <option value="4">Etiqueta 3</option>
             </select>
             <br>
             <select class="form-select" name="etiqueta2" aria-label="Default select example">
-                <option selected>Sin Etiqueta</option>
-                <option value="1">Etiqueta 1</option>
-                <option value="2">Etiqueta 2</option>
-                <option value="3">Etiqueta 3</option>
+                <option value="1">Sin Etiqueta</option>
+                <option value="2">Etiqueta 1</option>
+                <option value="3">Etiqueta 2</option>
+                <option value="4">Etiqueta 3</option>
             </select>
             <br>
             <select class="form-select" name="etiqueta3" aria-label="Default select example">
-                <option selected>Sin Etiqueta</option>
-                <option value="1">Etiqueta 1</option>
-                <option value="2">Etiqueta 2</option>
-                <option value="3">Etiqueta 3</option>
+                <option value="1">Sin Etiqueta</option>
+                <option value="2">Etiqueta 1</option>
+                <option value="3">Etiqueta 2</option>
+                <option value="4">Etiqueta 3</option>
             </select>
             <br>
-            <input type="text" name="usuario" value="<?php echo $_SESSION['ID'] ?>">
-            <div><button class="btn btn-primary">Publicar</button></div>
+            <div hidden>
+                <input type="text" name="usuario" value="<?php echo $_SESSION['ID'] ?>">
+                <button id="enviar"></button>
+            </div>
         </form>
+        <div><button id="btnEnvio" class="btn btn-primary">Publicar</button></div>
+
     </div>
 
 
     <footer>
 
     </footer>
-
+    <script src="../js/ValidadorTexto.js"></script>
+    <script src="../js/tema.js"></script>
     <script src="../bootstrap/js/bootstrap.min.js"></script>
 </body>
 
