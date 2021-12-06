aabad4a4e2f9939ffc06c5667d3ccd98f1d0248d
brayanbravo <brayan.bravo.arebalo@gmail.com>
2021-12-06 14:20:24 -0300

    integracion de plantillas para la visualizacion del contenido

--- pages/paginaPrincipal.php
+++ pages/paginaPrincipal.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+if (isset($_SESSION["user"])) {
+} else {
+    header("location:../pages/InicioSesion.php");
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -21,12 +30,24 @@
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
                         <ul class="navbar-nav">
-
-                            <li class="nav-item">
-                                <a class="nav-link active" href="">pagina 1</a>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Tema
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item" href="#">Mis Temas</a></li>
+                                    <li><a class="dropdown-item" href="#">Otros temas</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="../pages/tema.php">Iniciar Tema</a></li>
+                                </ul>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link active" href="">cuenta</a>
+                                <a class="nav-link active" href="">Mis Etiquetas</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="../pages/cerrarSesion.php">cerrar Sesion</a>
                             </li>
                         </ul>
                     </div>
@@ -34,6 +55,19 @@
             </div>
         </nav>
     </header>
+
+    <div id="contenido">
+        <div hidden>
+            <input id="iduser" type="text" value="<?php echo $_SESSION['ID'] ?>">
+        </div>
+        <div id=listaTemas>
+
+        </div>
+
+
+    </div>
+
+
 
 
     <footer>
@@ -47,8 +81,8 @@
             <div class="card-header">
 
                 <button type="button" class="btn btn-white position-relative">
-                    <h5 id="nombreTema"></h5>
-                    <span id="cantidad" class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                    <h5 class="nombreTema"></h5>
+                    <span class="cantidad" class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
                     </span>
                 </button>
 
@@ -56,7 +90,7 @@
             </div>
 
             <div class="card-body">
-                <p id="importancia"></p>
+                <p class="importancia"></p>
             </div>
 
             <div class="card-footer text-muted">
@@ -69,7 +103,49 @@
 
                 </div>
                 <div>
-                    <button class="btn btn-primary">agragar</button>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        Agragar
+                    </button>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <form action="" method="POST">
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control" name="titulo" id="floatingInput" placeholder=" ">
+                                            <label for="floatingInput">Titulo</label>
+                                        </div>
+                                        <div class="form-floating mb-3">
+                                            <textarea class="form-control" name="texto" placeholder="" id="floatingTextarea2" style="height: 400px"></textarea>
+                                            <label for="floatingTextarea2">Texto</label>
+                                        </div>
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control" name="cita" id="floatingInput2" placeholder=" ">
+                                            <label for="floatingInput2">Cita</label>
+                                        </div>
+                                        <br>
+                                        <div hidden>
+                                            <input id="idtema" type="text" name="idtema">
+                                            <button id="enviar"></button>
+                                        </div>
+
+
+
+
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button id="btnEnvio" type="button" class="btn btn-primary">publicar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <button class="btn btn-primary">Ant</button>
                     <button class="btn btn-primary">Sig</button>
                     <button class="btn btn-primary">Debatir</button>
@@ -84,17 +160,17 @@
 
         <div class="accordion-item">
             <h2 class="accordion-header" id="headingThree">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                    <h5 id="nombreSubtema"></h5>
+                <button id="btnAcordion" class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    <h5 class="nombreSubtema"></h5>
                 </button>
             </h2>
             <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
                 <div class="accordion-body">
                     <span class="badge bg-danger rounded-pill">
-                        <h6 id="nombreUsuario"></h6>
+                        <h6 class="nombreUsuario"></h6>
                     </span><br>
-                    <p id="Contenido"></p>
-                    <cite title="Source Title" id="cita"></cite>
+                    <p class="Contenido"></p>
+                    <cite title="Source Title" class="cita"></cite>
                 </div>
             </div>
         </div>
@@ -102,6 +178,7 @@
     </template>
 
     <script src="../bootstrap/js/bootstrap.min.js"></script>
+    <script src="../js/paginaPrincipal.js"></script>
 </body>
 
 </html>
