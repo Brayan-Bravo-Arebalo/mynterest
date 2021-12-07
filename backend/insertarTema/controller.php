e97fc3cde6d0f9dbc0ef70f2339638191b9ef3c2
brayanbravo <brayan.bravo.arebalo@gmail.com>
2021-12-07 13:11:35 -0300

    cambio menor (cambio entre saltos de linea)

--- backend/insertarTema/controller.php
+++ backend/insertarTema/controller.php
@@ -10,16 +10,14 @@ $usuario = $_POST["usuario"];
 
 if (!empty($titulo) && !empty($importancia) && !empty($usuario)) {
     $id = insertarTema($titulo, $importancia, $usuario, $etiqueta1, $etiqueta2, $etiqueta3);
-    if ($id =1) {
-        header("location:correcto.php");
+    if ($id = 1) {
+
+        header("location:../../pages/paginaPrincipal.php");
     } else {
         header("location:error.php");
     }
-  
 } else {
     header("location:error.php");
-
-    
 }
 
 
