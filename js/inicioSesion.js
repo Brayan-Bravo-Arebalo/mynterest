5ee3af434641b0fc2aa668294577d435d1980a01
brayanbravo <brayan.bravo.arebalo@gmail.com>
2021-12-07 13:31:42 -0300

    archivo para validacion de login

---
+++ js/inicioSesion.js
@@ -0,0 +1,9 @@
+const clave = document.getElementById("floatingPassword");
+const btnLogin = document.getElementById("btnLogin");
+const login = () => {
+
+    let clave2 = document.getElementById("password2")
+    //clave2.value = md5(clave.value)
+    clave2.value = clave.value
+    
+}
