3481559e5d8a581ab4dfd4cc691d5e0908e9ee83
brayanbravo <brayan.bravo.arebalo@gmail.com>
2021-12-10 13:40:57 -0300

    habilitar encriptado md5 en inicio de sesion

--- js/inicioSesion.js
+++ js/inicioSesion.js
@@ -3,7 +3,7 @@ const btnLogin = document.getElementById("btnLogin");
 const login = () => {
 
     let clave2 = document.getElementById("password2")
-    //clave2.value = md5(clave.value)
-    clave2.value = clave.value
+    clave2.value = md5(clave.value)
+    //clave2.value = clave.value
     
 }
